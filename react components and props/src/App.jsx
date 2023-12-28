1e560440642544c00d535bfbc6820ce457db3903
In28 <ina.moisejeva@gmail.com>
2023-12-28 15:28:24 +0200

    namu pratimas rc

---
+++ react components and props/src/App.jsx
@@ -0,0 +1,56 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="title">
+      <h1>Title</h1>
+      <p>Some about text</p>
+      <p>in two lines</p>
+      <button>Button</button>
+     </div>
+
+     <div class="portfolio">
+  <h1>Portfolio</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel fermentum elit. Sed gravida est vitae libero viverra interdum. Ut nec semper odio. Sed vehicula convallis libero, sit amet sollicitudin quam pharetra non.
+  </p>
+  <div class="card-container">
+    <div class="card">
+      <img src="../src/img/2024.jpg" alt="" />
+      <h4>Ipsum Feugiat</h4>
+    </div>
+    <div class="card">
+      <img src="../src/img/2024.jpg" alt="" />
+      <h4>Ipsum Feugiat</h4>
+    </div>
+    <div class="card">
+      <img src="../src/img/2024.jpg" alt="" />
+      <h4>Ipsum Feugiat</h4>
+    </div>
+  </div>
+
+  <div class="card-container">
+    <div class="card">
+      <img src="../src/img/smile.avif" alt="" />
+      <h4>Ipsum Feugiat</h4>
+    </div>
+    <div class="card">
+    <img src="../src/img/smile.avif" alt="" />
+      <h4>Ipsum Feugiat</h4>
+    </div>
+    <div class="card">
+    <img src="../src/img/smile.avif" alt="" />
+      <h4>Ipsum Feugiat</h4>
+    </div>
+  </div>
+</div>
+
+
+
+    </>
+  )
+}
+
+export default App
